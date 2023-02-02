c4b66abafb5cd2eb82eff41ce158fcfa8eacbf1c
Wu Mianzhi <1427207878@qq.com>
2023-02-02 15:54:46 +0800

    init project

---
+++ include/U8String.h
@@ -0,0 +1,8 @@
+//
+// Created by Wu Mianzhi on 2023/2/2.
+//
+
+#ifndef C_SQL_U8STRING_H
+#define C_SQL_U8STRING_H
+
+#endif //C_SQL_U8STRING_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Wu Mianzhi on 2023/2/2.
+//
+#include <iostream>
+
+int main()
+{
+    std::cout<<"hello world!"<<std::endl;
+    return 0;
+}
