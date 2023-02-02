391c59caacf73c3d390c084748ef0ccf4d7af9d2
Wu Mianzhi <1427207878@qq.com>
2023-02-02 16:29:00 +0800

    init utf8 string class

---
+++ src/U8String.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Wu Mianzhi on 2023/2/2.
+//
+
+#include "../include/U8String.h"
+
+U8String::U8String()
+{
+    ;
+}
+
+string U8String::toRawBytes()
+{
+    ;
+}
+
+std::istream& operator>>(std::istream& istream, U8String u8string)
+{
+    ;
+}
